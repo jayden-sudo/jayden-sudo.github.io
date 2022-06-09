ab7ec2e90aff699cda865bd21b3a1579ad622179
zhangshengjie <z.cejay@gmail.com>
2022-06-09 11:04:36 +0800

    save

---
+++ 3057.9fce0c8be89b06d6.js
@@ -0,0 +1,4 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[3057],{3057:(h,n,t)=>{t.r(n),t.d(n,{startStatusTap:()=>r});var d=t(5861),o=t(7205),_=t(6465),i=t(3756);
+/*!
+     * (C) Ionic http://ionicframework.com - MIT License
+     */const r=()=>{const s=window;s.addEventListener("statusTap",()=>{(0,o.f)(()=>{const a=document.elementFromPoint(s.innerWidth/2,s.innerHeight/2);if(!a)return;const e=(0,_.f)(a);e&&new Promise(c=>(0,i.c)(e,c)).then(()=>{(0,o.c)((0,d.Z)(function*(){e.style.setProperty("--overflow","hidden"),yield(0,_.s)(e,300),e.style.removeProperty("--overflow")}))})})})}}}]);
